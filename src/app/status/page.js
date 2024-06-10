fef3afa247edab49db818fe0eb8458f690afcc5e
leonard adjei <157623061+leonardeganow@users.noreply.github.com>
2024-06-10 15:53:27 +0000

    clearing local storage on success or fail

--- src/app/status/page.js
+++ src/app/status/page.js
@@ -7,52 +7,58 @@ import { FaCheckCircle } from "react-icons/fa";
 import { MdCancel } from "react-icons/md";
 // import { useRouter } from 'next/router'
 import { useParams, useRouter, useSearchParams } from "next/navigation";
-import React, { Suspense } from "react";
-export const dynamic = 'force-dynamic'
+import React, { Suspense, useEffect } from "react";
+export const dynamic = "force-dynamic";
 function Page() {
+  //   const params = useSearchParams();
 
-//   const params = useSearchParams();
+  //   const status = params.get("status");
+  //   // const params = useRouter()
+  // console.log(status);
 
-//   const status = params.get("status");
-//   // const params = useRouter()
-// console.log(status);
+  function PageContent() {
+    const params = useSearchParams();
+    const status = params.get("status");
 
-function PageContent() {
-  const params = useSearchParams();
-  const status = params.get("status");
+    useEffect(() => {
+      //clear local storage
+      localStorage.clear();
 
-  return (
-    <>
-      <DefaultHeader />
-      <MobileMenu />
-      <section className="bgc-f7">
-        <div className="d-flex justify-content-center">
-          {status === "success" ? (
-            <FaCheckCircle className="text-success" size={70} />
-          ) : (
-            <MdCancel className="text-danger" size={70} />
-          )}
-        </div>
-        <h1 className="text-center my-4">
-          Your payment {status === "success" ? "was successful" : "failed"}
-        </h1>
-        <div className="d-flex justify-content-center">
-        <Link href="/" className="ud-btn btn-dark">
-                  Go Back To Homepages
-                  <i className="fal fa-arrow-right-long" />
-                </Link>
-        </div>
-      </section>
-      <section className="footer-style1 pt60 pb-0">
-        <Footer />
-      </section>
-    </>
-  );
-}
+      //clear local storage
+    },[]);
+
+    return (
+      <>
+        <DefaultHeader />
+        <MobileMenu />
+        <section className="bgc-f7">
+          <div className="d-flex justify-content-center">
+            {status === "success" ? (
+              <FaCheckCircle className="text-success" size={70} />
+            ) : (
+              <MdCancel className="text-danger" size={70} />
+            )}
+          </div>
+          <h1 className="text-center my-4">
+            Your payment {status === "success" ? "was successful" : "failed"}
+          </h1>
+          <div className="d-flex justify-content-center">
+            <Link href="/" className="ud-btn btn-dark">
+              Go Back To Homepages
+              <i className="fal fa-arrow-right-long" />
+            </Link>
+          </div>
+        </section>
+        <section className="footer-style1 pt60 pb-0">
+          <Footer />
+        </section>
+      </>
+    );
+  }
   return (
     <Suspense fallback={<p>...loading</p>}>
-    <PageContent />
-  </Suspense>
+      <PageContent />
+    </Suspense>
   );
 }
 
